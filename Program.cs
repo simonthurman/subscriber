4c9347eb45ef825b25e11fbe3c1309aa80edc0e7
Simon Thurman <simont@microsoft.com>
2021-08-16 16:51:18 +0100

    Add Function call

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Net.Http;
 using Azure.Messaging.WebPubSub;
 using Websocket.Client;
 
@@ -20,12 +21,15 @@ namespace subscriber
             // Either generate the URL or fetch it from server or fetch a temp one from the portal
             var serviceClient = new WebPubSubServiceClient(connectionString, hub);
             var url = serviceClient.GenerateClientAccessUri();
+            var funkyClient = new HttpClient();
 
             using (var client = new WebsocketClient(url))
             {
                 // Disable the auto disconnect and reconnect because the sample would like the client to stay online even no data comes in
                 client.ReconnectTimeout = null;
-                client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+                //client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+
+                client.MessageReceived.Subscribe(msg => funkyClient.GetAsync("https://processfunky.azurewebsites.net/api/process?code=JVhW7xZC6ju81y4ZfCZS3D2V8lfnuAzu2RbkaCYPzXbuUat1xgPvKw=="));
                 await client.Start();
                 Console.WriteLine("Connected.");
                 Console.Read();
