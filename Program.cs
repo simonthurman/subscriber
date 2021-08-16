dff5bb30eb10e9e6e0430538c305f5546123302e
Simon Thurman <simont@microsoft.com>
2021-08-16 16:25:49 +0100

    inital

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.WebPubSub;
+using Websocket.Client;
 
 namespace subscriber
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: subscriber <connectionString> <hub>");
+                return;
+            }
+            var connectionString = args[0];
+            var hub = args[1];
+
+            // Either generate the URL or fetch it from server or fetch a temp one from the portal
+            var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+            var url = serviceClient.GenerateClientAccessUri();
+
+            using (var client = new WebsocketClient(url))
+            {
+                // Disable the auto disconnect and reconnect because the sample would like the client to stay online even no data comes in
+                client.ReconnectTimeout = null;
+                client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+                await client.Start();
+                Console.WriteLine("Connected.");
+                Console.Read();
+            }
         }
     }
 }
